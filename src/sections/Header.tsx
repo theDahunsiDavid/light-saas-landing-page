bec4ba100c48e40d1aed9b8cc4ef51934faf3eb0
theDahunsiDavid <gbemigadare@gmail.com>
2025-12-07 08:11:17 +0100

    feat: Complete banner for mobile view.

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg";
+
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center bg-black text-white py-3">
+      <div className="inline-flex gap-1 items-center">
+        <p>Get started for free</p>
+        <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+      </div>
+    </div>
+  );
 };
